9c505f9e467fc78ed39d4c3f5bcc197f0bae71a2
srtalaie <srt4721@yahoo.com>
2019-03-07 11:00:45 -0800

    set up store and wrap it over App

---
+++ client/public/store/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createStore, combineReducers } from 'redux';
+import reducers from '../reducers/index.js';
+
+export const store = createStore(
+    combineReducers({
+        state: reducers
+    })
+);
