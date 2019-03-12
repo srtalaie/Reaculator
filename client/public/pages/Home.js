d71aa0b7f868270d326972dc5faf9178d11dd22d
srtalaie <srt4721@yahoo.com>
2019-03-12 11:49:33 -0700

    create page that will show calculator

---
+++ client/public/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ButtonsCard from "../components/ButtonsCard/ButtonsCard.js";
+
+class Home extends React {
+    
+    render(){
+        return(
+            <div>
+                <input type="text"></input>
+            </div>
+    )}
+
+}
+
+export default Home;
