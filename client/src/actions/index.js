4cab99a243d53205897f702c778bc75b24b21e72
srtalaie <srt4721@yahoo.com>
2019-03-14 15:59:38 -0700

    wrote actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,12 @@
+export const addElement = (text)=> {
+    type: 'ADD_ELEM',
+    text: text
+}
+
+export const clear = () => {
+    type: 'CLEAR'
+}
+
+export const equals = () => {
+    type: 'EQUALS'
+}
